badbd61217b1db005701d1a02e237b46a7171827
Jack Bashford <jack@jackbashford.com>
2025-02-06 15:57:07 +1100

    init

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            ghc
+            haskell-language-server
+            just
+            watchexec
+          ];
+        };
+      }
+    );
+}
